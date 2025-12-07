8198f161529ce2cf3dfbf80d4be1effc26c939c7
Murat Kadir YILMAZ <kadrylmaz44@gmail.com>
2025-12-07 17:40:13 +0300

    Refactor output format and add graph header
    Updated the output format and added a header for the graph.

--- ciro_yatay.c
+++ ciro_yatay.c
@@ -14,18 +14,19 @@ int main(){
     srand(time(NULL)); // Rastegele sayı üretmek için zaman bazlı fonksiyon.
     
     int dizi[12];
-    printf("12 Aylik ciro hesaplaniyor...\n\n");
+    
     
     for(int i = 0; i < 12; i++){ // Rastgele ciro değerlerini oluşturur.
         dizi[i] = rand() %10 + 1; // 1 ile 10 arasında.
     }
+    printf("### Ciro Grafigi (Yatay Gorunum) ###\n\n");
+    printf("12 Aylik ciro hesaplaniyor...\n\n");
     printf("12 Aylik ciro (1K - 10K):\n");
     for(int i = 0; i < 12; i++){ // Ciro değerlerini yazdır.
         printf("%2d ",dizi[i]); 
     }
     printf("\n\n");
-    printf("Ciro Grafigi (Yatay Gorunum):\n\n");
-    
+
     for(int i = 0; i < 12; i++){ // Yatay grafik oluşturma.
         printf("%02d.:",i+1); // Ay numarasını iki haneli yazdır.
     
